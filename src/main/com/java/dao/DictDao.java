ab2efa532ec9e423234ab0ec3b8bf450faaeb919
linliangfang <linliangfang@bestpay.com.cn>
2017-09-26 09:23:12 +0800

    开发标签数据字典_DictDao

--- src/main/com/java/dao/DictDao.java
+++ src/main/com/java/dao/DictDao.java
@@ -6,19 +6,13 @@ import java.sql.ResultSet;
 
 import com.java.model.PageBean;
 import com.java.model.Dict;
-import com.java.util.StringUtil;
+
 
 public class DictDao {
 
 
-	public ResultSet userList(Connection con,PageBean pageBean,Dict user)throws Exception{
-		StringBuffer sb=new StringBuffer("select * from t_user u ,t_role r where u.roleId=r.roleId and u.userType!=1 ");
-		if(StringUtil.isNotEmpty(user.getUserName())){
-			sb.append(" and u.userName like '%"+user.getUserName()+"%'");
-		}
-		if(user.getRoleId()!=-1){
-			sb.append(" and u.roleId="+user.getRoleId());
-		}
+	public ResultSet userList(Connection con,PageBean pageBean,Dict dict)throws Exception{
+		StringBuffer sb=new StringBuffer("select * from t_sym_dict u  ");
 		if(pageBean!=null){
 			sb.append(" limit "+pageBean.getStart()+","+pageBean.getRows());
 		}
@@ -27,14 +21,8 @@ public class DictDao {
 	}
 	
 	
-	public int userCount(Connection con,User user)throws Exception{
-		StringBuffer sb=new StringBuffer("select count(*) as total from t_user u ,t_role r where u.roleId=r.roleId and u.userType!=1 ");
-		if(StringUtil.isNotEmpty(user.getUserName())){
-			sb.append(" and u.userName like '%"+user.getUserName()+"%'");
-		}
-		if(user.getRoleId()!=-1){
-			sb.append(" and u.roleId="+user.getRoleId());
-		}
+	public int userCount(Connection con,Dict dict)throws Exception{
+		StringBuffer sb=new StringBuffer("select count(*) as total from t_sym_dict u ");
 		PreparedStatement pstmt=con.prepareStatement(sb.toString());
 		ResultSet rs=pstmt.executeQuery();
 		if(rs.next()){
@@ -44,31 +32,31 @@ public class DictDao {
 		}
 	}
 	
-	public int userAdd(Connection con,User user)throws Exception{
-		String sql="insert into t_user values(null,?,?,2,?,?)";
-		PreparedStatement pstmt=con.prepareStatement(sql);
-		pstmt.setString(1, user.getUserName());
-		pstmt.setString(2, user.getPassword());
-		pstmt.setInt(3, user.getRoleId());
-		pstmt.setString(4, user.getUserDescription());
-		return pstmt.executeUpdate();
-	}
-	
-	public int userUpdate(Connection con,User user)throws Exception{
-		String sql="update t_user set userName=?,password=?,roleId=?,userDescription=? where userId=?";
-		PreparedStatement pstmt=con.prepareStatement(sql);
-		pstmt.setString(1, user.getUserName());
-		pstmt.setString(2, user.getPassword());
-		pstmt.setInt(3, user.getRoleId());
-		pstmt.setString(4, user.getUserDescription());
-		pstmt.setInt(5, user.getUserId());
-		return pstmt.executeUpdate();
-	}
+//	public int userAdd(Connection con,User user)throws Exception{
+//		String sql="insert into t_user values(null,?,?,2,?,?)";
+//		PreparedStatement pstmt=con.prepareStatement(sql);
+//		pstmt.setString(1, user.getUserName());
+//		pstmt.setString(2, user.getPassword());
+//		pstmt.setInt(3, user.getRoleId());
+//		pstmt.setString(4, user.getUserDescription());
+//		return pstmt.executeUpdate();
+//	}
+//
+//	public int userUpdate(Connection con,User user)throws Exception{
+//		String sql="update t_user set userName=?,password=?,roleId=?,userDescription=? where userId=?";
+//		PreparedStatement pstmt=con.prepareStatement(sql);
+//		pstmt.setString(1, user.getUserName());
+//		pstmt.setString(2, user.getPassword());
+//		pstmt.setInt(3, user.getRoleId());
+//		pstmt.setString(4, user.getUserDescription());
+//		pstmt.setInt(5, user.getUserId());
+//		return pstmt.executeUpdate();
+//	}
 	
 
 	
-	public int userDelete(Connection con,String delIds)throws Exception{
-		String sql="delete from t_user where userId in ("+delIds+")";
+	public int userDelete(Connection con,String dictId)throws Exception{
+		String sql="delete from t_sym_dict where dict_id in ("+dictId+")";
 		PreparedStatement pstmt=con.prepareStatement(sql);
 		return pstmt.executeUpdate();
 	}
